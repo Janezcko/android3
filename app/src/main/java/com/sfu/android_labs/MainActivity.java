8e76d3b8d0312d174c9f2b69512c47fc1df73dd4
Anton <lazarevich-anton@mail.ru>
2021-10-23 05:25:52 +0700

    Added all

---
+++ app/src/main/java/com/sfu/android_labs/MainActivity.java
@@ -0,0 +1,46 @@
+    package com.sfu.android_labs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.activity_main__rv_vacations);
+        VacationAdapter vacationAdapter = new VacationAdapter(generateMovieList());
+        recyclerView.setAdapter(vacationAdapter);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        FloatingActionButton fab = findViewById(R.id.activity_main__fb_add);
+        Intent intent = new Intent(MainActivity.this, UserProfileActivity.class);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+
+        private List<Vacation> generateMovieList() {
+            List<Vacation> list = new ArrayList<>();
+            list.add(new Vacation("some","dede",R.drawable.ic_launcher_background));
+            list.add(new Vacation("some","dede",R.drawable.ic_launcher_background));
+            return list;
+        }
+
+}
